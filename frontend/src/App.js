132b99ae604b4bcb51c270edc4ab6109d27096c8
shalinibalodi <shalini.balodi_ug23@ashoka.edu.in>
2023-12-10 20:51:32 +0530

    more frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from './components/Home'
+import Layout from './components/Layout'
+//import Navbar from './components/Header'
 import FabcoinWallet from './components/FabcoinWallet';
 import FabcoinTransaction from './components/FabcoinTransaction';
 import Registration from "./components/Registration"
 function App() {
   return (
+    <> 
     <div className="App">
-      <h1 >Fabric Client App</h1>
-      <Registration/>
-      <FabcoinWallet />
-      <FabcoinTransaction />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route exact path="/FabcoinWallet" element={<FabcoinWallet/>} />
+          <Route exact path="/Fabcointransaction" element={<FabcoinTransaction/>} />
+          <Route exact path="/Registration" element={<Registration />}/>
+          </Route>
+        </Routes>
+      </Router>
     </div>
+    </>
   );
 }
 
 
 export default App;
+
+//<Registration/>
+//<FabcoinWallet />
+//<FabcoinTransaction />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,22 @@
 import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+//import FabcoinWallet from './components/FabcoinWallet';
+//import FabcoinTransaction from './components/FabcoinTransaction';
+//import Registration from "./components/Registration"
 const Navbar = () => {
   return <>
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="secondary">
+      <AppBar position="static" style={{backgroundColor: "black", color: "secondary"}}>
         <Toolbar>
-          <Typography variant='h5' component="div" sx={{ flexGrow: 1 }}>FabCoin</Typography>
+          <Typography variant='h4' align="left" component="div" sx={{ flexGrow: 1 }}>FabCoin</Typography>
 
-          <Button component={NavLink} to='/' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Mint</Button>
+          <Button component={NavLink} to='/' style={({ isActive }) => { return { backgroundColor: isActive ? '#094237' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Home</Button>
 
-          <Button component={NavLink} to='/contact' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Spend</Button>
+          <Button component={NavLink} to='/FabcoinWallet' style={({ isActive }) => { return { backgroundColor: isActive ? '#094237' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Mint</Button>
 
-          <Button component={NavLink} to='/login' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Login/Registration</Button>
+          <Button component={NavLink} to='/FabcoinTransaction' style={({ isActive }) => { return { backgroundColor: isActive ? '#094237' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Spend</Button>
+
+          <Button component={NavLink} to='/Registration' style={({ isActive }) => { return { backgroundColor: isActive ? '#094237' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Login/Register</Button>
 
         </Toolbar>
       </AppBar>
@@ -20,3 +25,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+//#6d1b7b
